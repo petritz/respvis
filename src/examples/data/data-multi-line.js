e7ccc3cac971345ed9f85bf038306703b9be1b08
vadler1 <adler.valentin@gmail.com>
2021-06-14 17:38:03 +0200

    Multi Line Chart

---
+++ src/examples/data/data-multi-line.js
@@ -0,0 +1,22 @@
+// artificial dataset for multiline chart
+
+export default {
+    datasets: [
+        {
+            mainValues: [1,2,3,4,6],
+            crossValues: [543,234,343,321,455]
+        },
+        {
+            mainValues: [1,2,3,4,6],
+            crossValues: [102,432,222,433,200]
+        },
+        {
+            mainValues: [1,2,3,5,6],
+            crossValues: [600,500,400,300,200]
+        },
+        {
+            mainValues: [1,2,4,5,5.5],
+            crossValues: [100,200,300,100,500]
+        }
+    ]
+};
--- src/lib/points/index.ts
+++ src/lib/points/index.ts
@@ -2,3 +2,4 @@ export * from './series-point';
 export * from './chart-point';
 export * from './chart-point-connected';
 export * from './chart-line';
+export * from './chart-multi-line';
